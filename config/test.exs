7ce3b7d073c95f0e8b9a6d54fcd6ef86f0ff7811
Rodolfo Maspoch <rmaspoch@zytechsoft.com>
2025-08-03 16:27:52 -0400

    Added authentication

--- config/test.exs
+++ config/test.exs
@@ -1,4 +1,6 @@
 import Config
+config :tunez, token_signing_secret: "+eKIRP/Nb9t4JmFGdRwCA55b22yo0nTQ"
+config :bcrypt_elixir, log_rounds: 1
 config :ash, policies: [show_policy_breakdowns?: true]
 
 # Configure your database
